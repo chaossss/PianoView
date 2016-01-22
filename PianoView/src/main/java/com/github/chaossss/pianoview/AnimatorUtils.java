e25f20b39649419e14458e1f595d2cd059d44489
Chaos <437700255@qq.com>
2016-01-22 11:51:58 +0800

    update

--- PianoView/src/main/java/com/github/chaossss/pianoview/AnimatorUtils.java
+++ PianoView/src/main/java/com/github/chaossss/pianoview/AnimatorUtils.java
@@ -35,17 +35,6 @@ public class AnimatorUtils {
         return localObjectAnimator;
     }
 
-
-    /**
-     * 移动ScrollView的x轴
-     *
-     * @param view      要移动的ScrollView
-     * @param toX       要移动到的X轴坐标
-     * @param time      动画持续时间
-     * @param delayTime 延迟开始动画的时间
-     * @param isStart   是否开始动画
-     * @return
-     */
     public static Animator moveScrollViewToX(View view, int toX, int time, int delayTime, boolean isStart) {
         ObjectAnimator objectAnimator = ObjectAnimator.ofInt(view, "scrollX", new int[]{toX});
         objectAnimator.setDuration(time);
@@ -56,29 +45,6 @@ public class AnimatorUtils {
         return objectAnimator;
     }
 
-    /**
-     * 将View的背景颜色更改，使背景颜色转换更和谐的过渡动画
-     *
-     * @param view      要改变背景颜色的View
-     * @param preColor  上个颜色值
-     * @param currColor 当前颜色值
-     * @param duration  动画持续时间
-     */
-    public static void showBackgroundColorAnimation(View view, int preColor, int currColor, int duration) {
-        ObjectAnimator animator = ObjectAnimator.ofInt(view, "backgroundColor", new int[]{preColor, currColor});
-        animator.setDuration(duration);
-        animator.setEvaluator(new ArgbEvaluator());
-        animator.start();
-    }
-
-    /**
-     * @param view                需要设置动画的view
-     * @param translationY        偏移量
-     * @param animatorTime        动画时间
-     * @param isStartAnimator     是否开启指示器
-     * @param isStartInterpolator 是否开始动画
-     * @return 平移动画
-     */
     public static Animator showUpAndDownBounce(View view, int translationY, int animatorTime, boolean isStartAnimator, boolean isStartInterpolator) {
         ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(view, "translationY", translationY);
         if (isStartInterpolator) {
